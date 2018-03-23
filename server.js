7de7282d0ac2a775f1bba37be3c3a4d039f7adda
bnord <bnord01@gmail.com>
2018-03-23 22:29:11 +0100

    Initial setup

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+
+
+// Serve public content
+app.use(express.static(__dirname + '/pub'));
+
+// Serve bower components
+app.use('/libs', express.static(__dirname + '/bower_components'));
+
+// Listen on the port given by heroku
+var port = process.env.PORT || 3003;
+var ready = new Promise(function willListen(resolve, reject) {
+    app.listen(port, function didListen(err) {
+        if (err) {
+            reject(err);
+            return;
+        }
+        console.log('app.listen on http://localhost:%d', port);
+        resolve();
+    });
+});
+
+exports.ready = ready;
+exports.app = app;
