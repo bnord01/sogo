a660e554359d22a9c15861412ce84fd72acbae3d
Benedikt Nordhoff <b_nord01@uni-muenster.de>
2018-07-11 19:26:23 +0200

    Format

--- ai/sogo_test.py
+++ ai/sogo_test.py
@@ -9,6 +9,6 @@ class SogoTest(unittest.TestCase):
     def test(self):
         sogo = Sogo()
         s0 = sogo.initial_state()
-        for action in [0,1,0,1,0,1,0]:
+        for action in [0, 1, 0, 1, 0, 1, 0]:
             s0 = sogo.step(s0, action)
             print(sogo.terminal_state(s0), sogo.evaluate_state(s0))
